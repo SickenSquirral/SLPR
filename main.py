544fceecc17e906d6ce69ee3bc90b2cf762bd5eb
Tobias Kjelsrud Kulvik <45426743+SickenSquirral@users.noreply.github.com>
2024-03-19 10:36:31 +0100

    Comments in main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Importing the necessary libraries
 import numpy as np
 import cv2
 import pytesseract
@@ -11,14 +12,18 @@ import re
 import requests
 from flask import Flask, Response, render_template, jsonify
 
+# Providing the API KEY used with the service
+API_KEY = '2c82b7c5-260a-42fb-828c-3961436b6272'
+# API Endpoint URL that will be used to get vehicle information
+API_URL = 'https://www.vegvesen.no/ws/no/vegvesen/kjoretoy/felles/datautlevering/enkeltoppslag/kjoretoydata?kjennemerke='
 
-API_KEY = '2c82b7c5-260a-42fb-828c-3961436b6272' # API KEY
-API_URL = 'https://www.vegvesen.no/ws/no/vegvesen/kjoretoy/felles/datautlevering/enkeltoppslag/kjoretoydata?kjennemerke=' # API URL
-
+# Definition of Vehicle class to hold vehicle-specific data
 class Vehicle:
     def __init__(self, data):
+        # If data is None, no further processing is done in the constructor
         if data is None:
             return
+        # Parsing relevant fields from input data
         self.license_plate = data['kjoretoydataListe'][0]['kjoretoyId'].get('kjennemerke')
         self.type = data['kjoretoydataListe'][0]['godkjenning']['tekniskGodkjenning']['tekniskeData']['generelt']['tekniskKode'].get('kodeNavn')
         self.brand = data['kjoretoydataListe'][0]['godkjenning']['tekniskGodkjenning']['tekniskeData']['generelt']['merke'][0]['merke']
@@ -29,40 +34,47 @@ class Vehicle:
         self.weight = str(data['kjoretoydataListe'][0]['godkjenning']['tekniskGodkjenning']['tekniskeData']['vekter'].get('egenvekt')) + ' kg'
         self.first_registration = data['kjoretoydataListe'][0]['forstegangsregistrering'].get('registrertForstegangNorgeDato')
 
+# Function that makes a GET request to the built URL and returns JSON response if successful
 def get_vehicle_info(license_plate):
     headers = {
-        'SVV-Authorization': f'Apikey {API_KEY}'
+        'SVV-Authorization': f'Apikey {API_KEY}' # Inserting API key into header
     }
     response = requests.get(API_URL + license_plate, headers=headers)
-    if response.status_code == 200:
+    if response.status_code == 200: # HTTP OK status
         return response.json()
     else:
         return {}
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-modelpath=f'detect.tflite'
-lblpath='labelmap.txt'
-min_conf=0.3
-cap = cv2.VideoCapture(1)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 992)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 558)
+# Define paths and constants for the object detection model
+modelpath=f'detect.tflite' # Path to trained TensorFlow Lite model
+lblpath='labelmap.txt' # Path to label map file
+min_conf=0.3 # Minimum confidence value for detected objects
+cap = cv2.VideoCapture(0) # Open default camera
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 992) # Camera frame width
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 558) # Camera frame height
 
+# Load the TFLite model and allocate tensors
 interpreter = Interpreter(model_path=modelpath)
 interpreter.allocate_tensors()
+
+# Get input and output tensors and their shapes
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 height = input_details[0]['shape'][1]
 width = input_details[0]['shape'][2]
-
 float_input = (input_details[0]['dtype'] == np.float32)
 
+# Normalization parameters for the input image
 input_mean = 127.5
 input_std = 127.5
 
+# Read the label map file
 with open(lblpath, 'r') as f:
     labels = [line.strip() for line in f.readlines()]
 
+# Function to process the image and call the OCR method
 def ocr_process(license_plate_img):
     # Deblur the image
     license_plate_img = cv2.GaussianBlur(license_plate_img, (0, 0), 3)
@@ -89,13 +101,12 @@ def ocr_process(license_plate_img):
     dilated = cv2.dilate(inverted, kernel, iterations=1)
 
     # Use pytesseract to recognize the license plate number
-    # Specify the language and OCR Engine Mode (oem)
-    # oem 3 means the default, based on what is available.
     custom_config = r'--oem 3 --psm 6'
     result = pytesseract.image_to_string(Image.fromarray(dilated), config=custom_config)
 
     return result
 
+# Function to format the recognized license plate text
 def license_plate_formatter(string):
     # Find two letters followed by five digits
     match = re.search(r'([a-zA-Z]{2}).*?(\d{5})', string)
@@ -112,13 +123,14 @@ def license_plate_formatter(string):
 
     return formatted_string
 
+# Setup the Flask application
 app = Flask(__name__, static_url_path='/static')
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0  # Disable caching
 
 newest_vehicle = None
 
+# Function to generate video feed frames, detect and recognize license plates, call the API with recognized value
 def generate_frames():
-
     global newest_vehicle
     license_plate_counts = {}
     license_plate = "none"
@@ -207,21 +219,25 @@ def index():
 
 @app.route('/video_feed')
 def video_feed():
+    # returning video feed over HTTP
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/get_vehicle')
 def get_vehicle():
+    # returning vehicle data as JSON
     global newest_vehicle
     if newest_vehicle:
-        return jsonify(newest_vehicle.__dict__)
+        return jsonify(newest_vehicle.__dict__) 
     else:
         return jsonify({})
     
 @app.route('/reset_vehicle')
 def reset_vehicle():
+    # endpoint to manually reset the newest_vehicle variable
     global newest_vehicle
     newest_vehicle = None
     return jsonify({})
 
+# Main segment which starts Flask app
 if __name__ == '__main__':
-    app.run(debug=False, port=8080)
+    app.run(debug=True, port=8080)
